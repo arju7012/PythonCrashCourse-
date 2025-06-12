870e52efefd04fdb80d5cba8f60759eb0ad0dfdc
Arjun <arjun7012p@gmail.com>
2025-06-12 23:21:37 +0530

    Inherirance instances as attributes

--- Classes_9/electric_car.py
+++ Classes_9/electric_car.py
@@ -33,10 +33,28 @@ class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
 
     def __init__(self,make,model,year):
-        """Initilize attributes of the parent class."""
+        """Initilize attributes of the parent class.
+           Then initialize attributes specific to an electric car.
+        """
         super().__init__(make,model,year)
-        
+        self.battery = Battery()
+    
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery}-kWh battery")
+
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self,battery_size=40):
+        """Initilize the battery's attributes."""
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
 
 
 my_tiago = ElectricCar('tata','tiago',2024)
 print(my_tiago.get_descriptive_name())
+my_tiago.battery.describe_battery()
